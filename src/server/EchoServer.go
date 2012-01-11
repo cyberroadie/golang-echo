88cd69d6ede4b648f18f5bf06e15afde54e15a97
Olivier Van Acker <olivier@eggbox.metatao.net>
2012-01-11 22:51:00 +0000

    Finished implementing server

--- src/server/EchoServer.go
+++ src/server/EchoServer.go
@@ -5,28 +5,31 @@
 package main
 
 import (
-	"fmt"
 	"flag"
 	"net"
 	"os"
 	"syslog"
 	"io"
+	"log"
+	"encoding/binary"
 )
 
 type Settings struct {
-	string PortNumber 
-	string Hostname
-	string Message
-	bool Verbose
-	bool Sctp
+	Address *string
+	Message *string 
+	Verbose *bool 
+	Sctp bool 
 }
 
-var settings = Settings{
-	PortNumber: flag.String("p", "4242", "port number to listen to")
-	Hostname: flag.String("h", "127.0.0.1", "address to listen on") 
-}
+var (
+	settings = Settings {
+	    Address: flag.String("a", "127.0.0.1:4242", "address to listen on"),
+		Message: flag.String("m", "recv", "message to send"),
+		Verbose: flag.Bool("v", false, "extra logging"),	
+	}
+)
 
-func test(err os.Error, mesg string) {
+func Test(err os.Error, mesg string) {
     if err!=nil {
         syslog, _ := syslog.New(syslog.LOG_ERR, "echo server")
         _, err = io.WriteString(syslog, mesg)
@@ -34,19 +37,62 @@ func test(err os.Error, mesg string) {
     }
 }
 
-func main() {
-	flag.Parse()
-	fmt.Printf("Listening on: %v:%v\n", *settings.Hostname, *settings.PortNumber)
+func HandleClient(conn net.Conn, message *string) {
+	
+	size, sizeBytes := uint32(0), make([]byte, 4)
+	id, idBytes := uint32(0), make([]byte, 4)
+	
+	for {
+		// Get message length
+		if n, err := io.ReadFull(conn, sizeBytes); n == 0 && err == os.EOF {
+			break
+		} else if err != nil {
+			log.Printf("read size: %s (after %d bytes)", err, n)
+			break
+		}
+		// Get message id
+		if n, err := io.ReadFull(conn, idBytes); n == 0 && err == os.EOF {
+			break
+		} else if err != nil {
+			log.Printf("read id: %s (after %d bytes)", err, n)
+			break
+		}
+		
+		size = binary.BigEndian.Uint32(sizeBytes)
+		data := make([]byte, size)
+		if _, err := io.ReadFull(conn, data); err != nil {
+			log.Printf("read data: %s", err)
+			break
+		}
+		
+		// Send message id back as ACK
+		id = binary.BigEndian.Uint32(idBytes)
+		//log.Printf("write data: %d", id)
+		if _, err := conn.Write(idBytes); err != nil {
+			log.Printf("write data: %d", id)
+			log.Printf("write data: %s", err)
+			break
+		}
+	}					
+}
 
-    netlisten, err := net.Listen("tcp", *settings.Hostname, *settings.PortNumber)
-    test(err, "Listen() error")
+func EchoServer(Address *string, Message *string) {
+	
+	netlisten, err := net.Listen("tcp", *settings.Address)
+	Test(err, "Listen() error")
     defer netlisten.Close()
 
-    conn, err := netlisten.Accept();
-    test(err, "Accept() error")
+	for {
+	    c, err := netlisten.Accept();
+    	Test(err, "Accept() error")
 
-    buf := make([]byte, 1024)
-    conn.Read(buf)
-    fmt.Println(string(buf))
+        go HandleClient(c, Message)
+    	
 
+	}	
+}
+
+func main() {
+	flag.Parse()
+	EchoServer(settings.Address, settings.Message)
 }
